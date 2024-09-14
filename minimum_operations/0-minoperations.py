e1c5c8b84dc7bc695a14316c92443fa7d13cccb6
Flo-renc <f.kabeya@alustudent.com>
2024-09-14 13:02:03 +0300

    documented the module in file 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
+"""
+minoperations module
+=====================
 
+This module provides a function `minOperations` that calculates the minimum 
+number of operations required to generate exactly `n` 'H' characters in a 
+text file starting with a single 'H'. The only operations allowed are:
+
+    1. Copy All
+    2. Paste
+
+    The solution is based on breaking down the problem using prime factorization 
+    to ensure the least number of operations.
+
+    Functions:
+        ----------
+        - minOperations(n): Returns the minimum number of operations needed to 
+          achieve exactly `n` 'H' characters.
+"""
 def minOperations(n):
     if n <= 1:
         return 0
