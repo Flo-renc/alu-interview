073f05f041f919518d7912623e5914f8b4ae68d0
Flo-renc <f.kabeya@alustudent.com>
2024-09-29 08:43:32 +0300

    corrections

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -2,13 +2,16 @@
 """
 0-rain Module
 
-This module provides a function that calculates how much rainwater will be retained between walls of varying heights, represented as a list of non-negative integers. 
+This module provides a function that calculates how much rainwater will
+beretained between walls of varying heights, represented as a list of non-negative integers.
 
-Each integer in the list corresponds to the height of a wall, and the amount of rainwater retained is calculated based on how water would accumulate between the walls after a rainfall.
+Each integer in the list corresponds to the height of a wall, and the amount of rainwater
+retained is calculated based on how water would accumulate between the walls after a rainfall.
 
 Function:
         rain(walls): Calculates the total amount of water retained between walls after it rains.
 """
+
 
 def rain(walls):
     """
@@ -17,7 +20,7 @@ def rain(walls):
         Return: integer indicating total amount of rainwater retained
     """
 
-    if not walls:
+    if not walls or len(walls) < 3:
         return 0
 
     n = len(walls)
@@ -26,15 +29,14 @@ def rain(walls):
 
     leftMax[0] = walls[0]
     for i in range(1, n):
-        leftMax[1] = max(leftMax[i-1], walls[1])
+        leftMax[1] = max(leftMax[i - 1], walls[1])
 
-    rightMax[n-1] = walls[n-1]
+    rightMax[-1] = walls[-1]
     for i in range(n -2, -1, -1):
         rightMax[i] = max(rightMax[i + 1], walls[i])
 
     total_retention = 0
     for i in range(n):
-        total_retention += min(leftMax[i], rightMax[i] - walls[i])
-
+        total_retention += max(0, min(leftMax[i], rightMax[i]) - walls[i])
 
     return total_retention
