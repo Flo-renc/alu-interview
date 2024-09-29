1f09fdad1143f8922afef8235e6d384784922a4c
Flo-renc <f.kabeya@alustudent.com>
2024-09-29 08:31:53 +0300

    rain retention

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+0-rain Module
+
+This module provides a function that calculates how much rainwater will be retained between walls of varying heights, represented as a list of non-negative integers. 
+
+Each integer in the list corresponds to the height of a wall, and the amount of rainwater retained is calculated based on how water would accumulate between the walls after a rainfall.
+
+Function:
+        rain(walls): Calculates the total amount of water retained between walls after it rains.
+"""
+
 def rain(walls):
     """
         Calculate how much water will be retained after it rains.
