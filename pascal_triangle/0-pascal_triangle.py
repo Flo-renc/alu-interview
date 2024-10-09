ee0c102788a0bc40b9f7cf0314df955832b524db
Flo-renc <f.kabeya@alustudent.com>
2024-10-09 04:48:24 +0300

    pascal triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -4,15 +4,15 @@
 
 def pascal_triangle(n):
     """
-    generates pascal's triangle of height n 
+    generates pascal's triangle of height n
     """
-    if n <=0:
+    if n <= 0:
         return []
 
     triangle = []
 
     for i in range(n):
-        row = [1] * (i + 1) #start with a row of 1's
+        row = [1] * (i + 1)  # start with a row of 1's
         if i > 1:
             for j in range(1, i):
                 row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
